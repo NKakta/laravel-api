8eeaebe1ea4ce1861c5eb97244ca37353236a925
nedaskakta <nedas.kakta@hostinger.com>
2021-03-10 16:23:45 +0200

    Remove header

--- src/public/index.php
+++ src/public/index.php
@@ -48,7 +48,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 $response = tap($kernel->handle(
