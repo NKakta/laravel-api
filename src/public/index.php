3a5dafb8dbc528490c2c8adfc7a25e8a2ce98b0f
nedaskakta <nedas.kakta@hostinger.com>
2021-03-10 16:22:26 +0200

    Remove header allow methods

--- src/public/index.php
+++ src/public/index.php
@@ -49,7 +49,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+//header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 $response = tap($kernel->handle(
     $request = Request::capture()
