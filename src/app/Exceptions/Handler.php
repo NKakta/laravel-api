c08aaa39b7caf43d8d5aaed081693622cf470fdc
nedaskakta <nedas.kakta@hostinger.com>
2021-03-11 15:42:30 +0200

    Add logging

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ namespace App\Exceptions;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
@@ -40,6 +41,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        Log::error('Exception received', [
+            'trace' => $exception->getTrace(),
+            'message' => $exception->getMessage(),
+            'line' => $exception->getLine()
+        ]);
         return ResponseHandler::response($exception);
     }
 
