7ff64403008d8a49fd820ec9b0db3d3f0c38bbde
nedaskakta <nedas.kakta@hostinger.com>
2021-03-11 20:18:52 +0200

    debug

--- src/app/Http/Controllers/V1/StatusController.php
+++ src/app/Http/Controllers/V1/StatusController.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\ApiController;
-use Illuminate\Http\Request;
 
 class StatusController extends ApiController
 {
-    public function update(Request $request, int $gameId)
+    public function update()
     {
         return 'okay';
     }
