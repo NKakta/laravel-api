31873e65d217b3b70a39343cbbf711ed2feade35
nedaskakta <nedas.kakta@hostinger.com>
2021-03-11 16:49:18 +0200

    debug

--- src/public/index.php
+++ src/public/index.php
@@ -50,6 +50,8 @@ $kernel = $app->make(Kernel::class);
 
 //header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Content-Type: application/json');
+
 
 $response = tap($kernel->handle(
     $request = Request::capture()
