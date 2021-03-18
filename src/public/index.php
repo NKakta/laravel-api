c20133bebe62e16a1a83921288004a84690a5abb
nedaskakta <nedas.kakta@hostinger.com>
2021-03-18 21:25:40 +0200

    Debug

--- src/public/index.php
+++ src/public/index.php
@@ -48,7 +48,6 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-var_dump(fopen('php://input', 'r'));die;
 $response = tap($kernel->handle(
     $request = Request::capture()
 ))->send();
