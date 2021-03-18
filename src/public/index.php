75b72ce0eca4931819511325ec0da0a46d362e0e
nedaskakta <nedas.kakta@hostinger.com>
2021-03-18 21:15:32 +0200

    Debug

--- src/public/index.php
+++ src/public/index.php
@@ -48,9 +48,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-
+echo 'vienas';die;
 $response = tap($kernel->handle(
     $request = Request::capture()
 ))->send();
