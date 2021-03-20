b43bf44b1aa95e456e500f3e6b4995518330ec48
nedaskakta <nedas.kakta@hostinger.com>
2021-03-20 20:01:05 +0200

    Add game status request usage

--- src/app/Http/Controllers/V1/GameStatusController.php
+++ src/app/Http/Controllers/V1/GameStatusController.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\ApiController;
+use App\Http\Requests\CreateGameStatusRequest;
 use App\Models\Activity;
 use App\Models\GameStatus;
 use App\Services\Activity\ActivityService;
 use App\Services\Game\GameService;
 use Illuminate\Http\JsonResponse;
 use \Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Request;
 
 class GameStatusController extends ApiController
 {
@@ -30,7 +30,7 @@ class GameStatusController extends ApiController
         $this->gameService = $gameService;
     }
 
-    public function update(Request $request, int $gameId): JsonResponse
+    public function update(CreateGameStatusRequest $request, int $gameId): JsonResponse
     {
         $status = GameStatus::where([
             'game_id' => $gameId,
@@ -41,15 +41,15 @@ class GameStatusController extends ApiController
         if (!$status instanceof GameStatus) {
             $status = GameStatus::create([
                 'game_id' => $gameId,
-                'status' => $request->get('status')
+                'status' => $request->status
             ]);
         }
 
-        if ($status->status === $request->get('status')) {
+        if ($status->status === $request->status) {
             return $this->errorResponse('Status cant be the same', 400);
         }
 
-        $status->status = $request->get('status');
+        $status->status = $request->status;
         $status->updateTimestamps();
         $status->save();
 
